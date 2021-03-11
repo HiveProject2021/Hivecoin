2aadc5a17534d7eccb7a064d332e8571c2993277
HiveProject2021 <jwang123@nyit.edu>
2021-03-11 11:56:20 -0800

    Update hive_zh_CN.ts

--- src/qt/locale/hive_zh_CN.ts
+++ src/qt/locale/hive_zh_CN.ts
@@ -901,7 +901,7 @@
 		</message>
 		<message>
 			<source>About %1</source>
-			<translation>關於 %1</translation>
+			<translation>关于 %1</translation>
 		</message>
 		<message>
 			<source>Command-line options</source>
