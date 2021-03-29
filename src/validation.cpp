5b625bfdf5405a527598ba99aa4de79ef1579375
HiveProject2021 <jwang123@nyit.edu>
2021-03-29 10:10:45 -0700

    Revert "Update validation.cpp"
    This reverts commit ed7fb4fc5db31352590d0e2d5b14fa771f5a4c39.

--- src/validation.cpp
+++ src/validation.cpp
@@ -2755,18 +2755,17 @@ static bool ConnectBlock(const CBlock& block, CValidationState& state, CBlockInd
 	
 	CAmount nCommunityAutonomousAmount 			= GetParams().CommunityAutonomousAmount();
 	CAmount nSubsidy 							= GetBlockSubsidy(pindex->nHeight, chainparams.GetConsensus());
-	CAmount nCommunityAutonomousAmountValue		= nSubsidy*nCommunityAutonomousAmount/100;
-	if(block.vtx[0]->vout[1].nValue != nCommunityAutonomousAmountValue)		{
+	if(block.vtx[0]->vout[1].nValue != (nSubsidy*nCommunityAutonomousAmount/100) )		{
 		return state.DoS(100,
                          error("ConnectBlock(): CommunityAutonomousAmount is not equal 10% of the Subsidy (actual=%ld vs shoulebe=%ld)", 
 							block.vtx[0]->vout[1].nValue, 
-							nCommunityAutonomousAmountValue
+							nSubsidy*nCommunityAutonomousAmount/100
 							),
                          REJECT_INVALID, "bad-cb-amount");
 	}
 	
 	LogPrintf("==>block.vtx[0]->vout[1].nValue: %ld \n", block.vtx[0]->vout[1].nValue);
-	LogPrintf("==>nCommunityAutonomousAmount: %ld \n", nCommunityAutonomousAmountValue);
+	LogPrintf("==>nCommunityAutonomousAmount: %ld \n", (nSubsidy*nCommunityAutonomousAmount/100));
 	LogPrintf("==>block.vtx[0]->vout[1].scriptPubKey: %s \n", block.vtx[0]->vout[1].scriptPubKey);
 	LogPrintf("==>GetCommunityAutonomousAddress: %s \n", GetCommunityAutonomousAddress);
 	LogPrintf("==>scriptPubKeyCommunityAutonomous: %s \n", HexStr(scriptPubKeyCommunityAutonomous));
