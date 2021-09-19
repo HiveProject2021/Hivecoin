af91c989f93868dce73378b09de4b27e0dd1b16c
tweety <lzhts@163.com>
2021-09-19 00:38:39 -0500

    update the build script

--- doc/build_with_db4_osx.sh
+++ doc/build_with_db4_osx.sh
@@ -6,11 +6,10 @@ HIVE_ROOT=$(pwd)
 CFLAGS="-Wno-error=implicit-function-declaration"  ./contrib/install_db4.sh .
 
 BDB_PREFIX="${HIVE_ROOT}/db4"
-export BDB_PREFIX='${BDB_PREFIX}'
 
 # Configure Hive Core to use our own-built instance of BDB
 cd $HIVE_ROOT
 ./autogen.sh
-./configure BDB_LIBS="-L${BDB_PREFIX}/lib -ldb_cxx-4.8" BDB_CFLAGS="-I${BDB_PREFIX}/include" --enable-cxx --disable-shared --disable-tests --disable-gui-tests
+./configure BDB_LIBS="-L${BDB_PREFIX}/lib -ldb_cxx-4.8" BDB_CFLAGS="-I${BDB_PREFIX}/include" LDFLAGS="-L${BDB_PREFIX}/lib/" CPPFLAGS="-I${BDB_PREFIX}/include/" --enable-cxx --disable-shared --disable-tests --disable-gui-tests
 make -j4
 make deploy
